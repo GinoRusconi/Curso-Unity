e685d4a94112d910315f9908e0a575e18e7e84c0
Gino Rusconi <ginorusconi@gmail.com>
2022-07-15 15:56:56 -0300

    Update Bomba.cs
    Activate Bomb in awake

--- Unity Inicial - Gino Rusconi/Assets/_Scripts/Bomba.cs
+++ Unity Inicial - Gino Rusconi/Assets/_Scripts/Bomba.cs
@@ -2,11 +2,34 @@ using UnityEngine;
 
 public class Bomba : MonoBehaviour
 {
+    public bool _Explotar;
+    public float _TimeToActivate;
+    private void Awake()
+    {
+        Invoke("ActivarBomba", _TimeToActivate);
+        _Explotar = false;
+
+    }
+
     void Update()
     {
         if (Input.GetButtonDown("Explotar"))
         {
-            Destroy(gameObject);
+            _Explotar = true;
+        }
+    }
+
+    void ActivarBomba()
+    {
+        _Explotar = true;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (_Explotar && (other.CompareTag("Player") || other.CompareTag("Enemy")))
+        {
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
         }
     }
 }
