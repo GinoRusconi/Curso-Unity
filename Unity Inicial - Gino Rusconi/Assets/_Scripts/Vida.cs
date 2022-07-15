9166b774c673a3cc1dec0318a0ff2e9414f2724d
Gino Rusconi <ginorusconi@gmail.com>
2022-07-15 16:00:32 -0300

    Create Vida.cs

---
+++ Unity Inicial - Gino Rusconi/Assets/_Scripts/Vida.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vida : MonoBehaviour
+{
+    public int _Cantidad;
+
+    void Update()
+    {
+        if (_Cantidad <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
