7e70ff9860f212c0700e3207711ee9da31dfcd30
Gino Rusconi <ginorusconi@gmail.com>
2022-07-15 15:59:22 -0300

    Update Disparador.cs

--- Unity Inicial - Gino Rusconi/Assets/_Scripts/Disparador.cs
+++ Unity Inicial - Gino Rusconi/Assets/_Scripts/Disparador.cs
@@ -4,12 +4,21 @@ public class Disparador : MonoBehaviour
 {
     public GameObject prefab;
     public string nombreAccion;
-    
+    public float _TimeBeetwenShoot;
     void Update()
     {
         if (Input.GetButtonDown(nombreAccion))
         {
-            Instantiate(prefab, transform.position, transform.rotation);
+            InvokeRepeating("Disparar", 0, _TimeBeetwenShoot);
         }
+        if (Input.GetButtonUp(nombreAccion))
+        {
+            CancelInvoke("Disparar");
+        }
+    }
+
+    void Disparar()
+    {
+        Instantiate(prefab, transform.position, transform.rotation);
     }
 }
